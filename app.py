f2b1f811281ca85c12e69d86c3eca7f7ef69b740
Fadhil Muhammed N C <ncfadhil@gmail.com>
2025-10-31 12:14:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -258,4 +258,7 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
     app.run(debug=True, host='0.0.0.0', port=5000)
